a24399acddb5826b8c82ca5d2eb491feb94081a2
Sakthivel M <68894066+sakthii02@users.noreply.github.com>
2021-06-28 10:20:11 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h2>SAS Programmer Professional Cert....</h2>
+      <p>Launch Your Career with a SAS® Credential. Master the skills required for the SAS® Ba...</p>
+    </div>
+  );
+}
+export default Note;
